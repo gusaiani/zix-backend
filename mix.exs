9756e6bea16b416ff58d8d804dbffb25c2569bfd
Gustavo Saiani <gustavo@poe.ma>
2018-06-06 17:19:35 +0200

    Add ExMachina and Comeonin to deps

--- mix.exs
+++ mix.exs
@@ -38,7 +38,9 @@ defmodule Zix.Mixfile do
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:ex_machina, "~> 2.1", only: :test},
+      {:comeonin, "~> 3.2"},
     ]
   end
 
