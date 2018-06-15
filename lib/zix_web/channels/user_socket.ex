f39105b3998a7dae5a4f66796d146d4d11733614
Gustavo Saiani <gustavo@poe.ma>
2018-06-15 18:48:28 +0200

    Prod settings

--- lib/zix_web/channels/user_socket.ex
+++ lib/zix_web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule ZixWeb.UserSocket do
   # channel "room:*", ZixWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport(:websocket, Phoenix.Transports.WebSocket, timeout: 45_000)
+  transport(:longpoll, Phoenix.Transports.LongPoll)
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
